83f4b49cee0d64daa57abd50e1cab52cb2c7bff5
Kevin Svetlitski <kevin_svetlitski@berkeley.edu>
2021-06-13 14:42:39 -0500

    Add more metadata to the dev_utils crate and update the pre-commit hook so that files under dev_utils are also formatted

--- .pre-commit.sh
+++ .pre-commit.sh
@@ -6,7 +6,10 @@ if comm -23 <(git diff --cached --name-only --diff-filter=CMRA | sort)\
             <(git diff --name-only --diff-filter=CMRA | sort)\
             | grep '\.rs$' > "$files_to_re_add"
 then
-	( cd "$(git rev-parse --show-toplevel)" && cargo fmt )
+	(
+	cd "$(git rev-parse --show-toplevel)" && cargo fmt
+	cd dev_utils && cargo fmt
+	)
 	xargs git add < "$files_to_re_add"
 fi
 
