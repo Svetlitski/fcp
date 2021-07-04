8ea1b8d65fb9d27adeeb80c454cdbe7363e7ae08
Kevin Svetlitski <kevin_svetlitski@berkeley.edu>
2021-07-03 16:20:10 -1000

    Add --version flag and update the help message

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,35 @@ use fcp::{fatal, fcp};
 use std::env;
 use std::process;
 
-static HELP: &str = "\
-fcp
-
+static HELP: &str = concat!(
+    "fcp ",
+    env!("CARGO_PKG_VERSION"),
+    "\n\n\
 USAGE:
-    fcp SOURCE DESTINATION_FILE
+    fcp [OPTIONS] SOURCE DESTINATION_FILE
     Copy SOURCE to DESTINATION_FILE, overwriting DESTINATION_FILE if it exists
 
-    fcp SOURCE ... DESTINATION_DIRECTORY
-    Copy each SOURCE into DESTINATION_DIRECTORY";
+    fcp [OPTIONS] SOURCE ... DESTINATION_DIRECTORY
+    Copy each SOURCE into DESTINATION_DIRECTORY
+
+OPTIONS:
+    -h, --help
+            Output this usage information and exit.
+
+    -V, --version
+            Output version information and exit."
+);
+
+static VERSION: &str = env!("CARGO_PKG_VERSION");
 
 fn main() {
     let args: Box<_> = env::args().skip(1).collect();
-    if args.iter().any(|arg| arg == "-h" || arg == "--help") {
-        fatal(HELP);
+    for arg in args.iter() {
+        match arg.as_str() {
+            "-h" | "--help" => fatal(HELP),
+            "-V" | "--version" => fatal(VERSION),
+            _ => {}
+        }
     }
     process::exit(fcp(&args) as i32);
 }
